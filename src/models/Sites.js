88fbdc94b80b9afad97ab6634986bcf5812d7060
GabrielDamian <damiangabriel54@yahoo.com>
2024-07-14 00:40:33 +0300

    Declare complete schema

--- src/models/Sites.js
+++ src/models/Sites.js
@@ -1,19 +1,24 @@
-// models/Site.js
 import mongoose from "mongoose";
 
 const SiteSchema = new mongoose.Schema({
-  index: {
-    type: Number,
-    required: true,
-  },
-  img: {
-    type: String,
-    required: true,
-  },
-  text: {
-    type: String,
-    required: true,
-  },
+  index: Number,
+  website_name: String,
+
+  hero_img: String,
+
+  section_2_img_1: String,
+  section_2_img_2: String,
+
+  room_1_name: String,
+  room_1_img: String,
+
+  room_2_name: String,
+  room_2_img: String,
+
+  room_3_name: String,
+  room_3_img: String,
+
+  parallax_bg_img: String,
 });
 
 export default mongoose.models.Site || mongoose.model("Site", SiteSchema);
